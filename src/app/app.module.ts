f1c8b866eaaeff74d5edc8fa5643a566983aac27
Charles Zhu <CharlesYZhu@yahoo.com>
2018-11-24 21:11:41 -0500

    started webpages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule } from '@angular/router'
+import { CarouselModule } from 'ngx-bootstrap/carousel'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProgramOverviewComponent } from './program-overview/program-overview.component';
+import { FacultyComponent } from './faculty/faculty.component';
+import { CurriculumComponent } from './curriculum/curriculum.component';
+
+const appRoutes: Routes = [
+  {path: '', component: ProgramOverviewComponent },
+  {path: 'faculty', component: FacultyComponent },
+  {path: 'curriculum', component: CurriculumComponent }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProgramOverviewComponent,
+    FacultyComponent,
+    CurriculumComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
+    CarouselModule.forRoot(),
     BrowserModule,
     AppRoutingModule
   ],
